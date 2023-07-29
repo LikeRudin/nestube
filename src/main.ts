5a5e84c1287d30c6c6bd401005b9ff0eea4dcae0
likeRudin <likeRudin@gmail.com>
2023-07-29 16:58:33 +0900

    add session option to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,38 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import "dotenv/config";
+import * as session from 'express-session';
+import * as MySQLStore from 'express-mysql-session';
+import * as mysql2 from "mysql2/promise";
+
 
 const PORT = process.env.PORT || 3000;
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const sessionOption = {
+    connectionLimit: 24 * 3600,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    createDatabaseTable: true
+  }
+
+  const connection = mysql2.createPool(sessionOption);
+  const sessionStore = new (MySQLStore(session))({}, connection);
+  
+
+
+  app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store:sessionStore
+  }));
   await app.listen(PORT);
   console.log(`server is running on http://localhost:${PORT}/`)
 }
