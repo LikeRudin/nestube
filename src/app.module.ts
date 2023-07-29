5500ff13f795c9231916430eeaf39ca66ef934d0
likeRudin <likeRudin@gmail.com>
2023-07-29 15:55:00 +0900

    connect to database with typeorm and mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,21 @@ import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
 import { UsersService } from './users/users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import "dotenv/config";
 
-@Module({
-  imports: [UsersModule],
+@Module({ 
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    entities: ['./users/entities/*.entity{.ts}'],
+    synchronize: false,
+  }),
+  UsersModule],
   controllers: [AppController, UsersController],
   providers: [AppService, UsersService],
 })
