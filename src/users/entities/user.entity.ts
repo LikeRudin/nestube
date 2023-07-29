8c1866d0f3688ab024c7a0cfce72e124700c0bd3
likeRudin <likeRudin@gmail.com>
2023-07-29 16:07:08 +0900

    create user entity, install class validator

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,23 @@
-export class User {}
+import { IsString } from "class-validator";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn,} from "typeorm";
+
+
+@Entity()
+export class UserEntity {
+    @PrimaryGeneratedColumn()
+    dbId:number;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+
+    @Column()
+    @IsString()
+    username: string;
+
+    @Column()
+    @IsString()
+    password: string;
+
+
+}
